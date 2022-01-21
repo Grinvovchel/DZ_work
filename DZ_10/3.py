cd0264d01e254909e6035a9f988ced1515e36cb7
Grinvovchel <Grinvovchel@gmail.com>
2022-01-21 15:07:03 +0300

    dz

---
+++ DZ_10/3.py
@@ -0,0 +1,32 @@
+class Cell:
+    def __init__(self, quantity):
+        self.quantity = int(quantity)
+
+    def __add__(self, other):
+        return f'Две клетки срослить. Теперь размер клетки равен: {self.quantity + other.quantity}'
+
+    def __sub__(self, other):
+        sub = self.quantity - other.quantity
+        return f'Клетка уменьшилась, теперь её размер равен: {sub} ' if sub > 0 else 'Клетки больше не существует'
+
+    def __truediv__(self, other):
+        return self.quantity // other.quantity
+
+    def __mul__(self, other):
+        return self.quantity * other.quantity
+
+    def make_order(self, row):
+        result = ''
+        for i in range(int(self.quantity / row)):
+            result += '*' * row + '\n'
+        result += '*' * (self.quantity % row) + '\n'
+        return result
+
+
+cell = Cell(45)
+cell_2 = Cell(7)
+print(cell + cell_2)
+print(cell - cell_2)
+print(cell / cell_2)
+print(cell * cell_2)
+print(cell.make_order(7))
